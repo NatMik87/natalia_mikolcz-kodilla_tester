4fcc687fc506a841f2eff6990232254af7490852
Natalia Mikolcz <natalia.mikolcz@uxpin.com>
2020-09-20 21:22:35 +0200

    new task with variable

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Natalia";
+        int myAge = 33;
+        double height = 1.68;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+    }
+}
---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String animal = "Giraffa";
+        String favoriteMovie = "Avatar";
+        int luckyNum = 5;
+        double temp = 36.6;
+        char size = 'M';
+        boolean sunnyWeather = true;
+        System.out.println(animal);
+        System.out.println(favoriteMovie);
+        System.out.println(luckyNum);
+        System.out.println(temp);
+        System.out.println(size);
+        System.out.println(sunnyWeather);
+    }
+}
