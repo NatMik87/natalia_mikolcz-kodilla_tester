a788c7967d1878ecde487ffeab10731cf9664071
Natalia Mikolcz <nmikolcz@gmail.com>
2020-10-25 14:09:48 +0100

    task Color with swich

---
+++ src/main/java/AdvCalculator.java
@@ -0,0 +1,30 @@
+public class AdvCalculator {
+    public static void main(String[] args) {
+        AdvCalculator calculator = new AdvCalculator();
+        double result = calculator.calculate();
+        System.out.println("Result: " + result);
+    }
+    public double calculate() {
+        String userSelected = UserDialogs.getUserSelection();
+        int a = UserDialogs.getValue();
+        int b = UserDialogs.getValue();
+        double result = 0;
+        switch (userSelected) {
+            case "ADD":
+                result = a + b;
+                break;                     // [1]
+            case "SUB":
+                result = a - b;
+                break;                     // [2]
+            case "DIV":
+                result = a / b;
+                break;                     // [3]
+            case "MUL":
+                result = a * b;
+                break;                     // [4]
+        }
+        return result;
+    }
+    }
+
+
---
+++ src/main/java/Colors.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Colors {
+    public static String getColor() {
+        String letter = Colors.getValue();
+        String result;
+        switch (getValue()) {
+            case "W":
+                result = "White";
+                break;
+            case "B":
+                result = "Black";
+                break;
+            case "R":
+                result = "Red";
+                break;
+            case "G":
+                result = "Green";
+                break;
+            default:
+                return "unknown";
+        }
+        return result;
+    }
+
+    public static String getValue() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first letter of color:");
+        String letter = scanner.next();
+        return letter;
+
+    }
+
+    public static void main(String[] args) {
+        Colors color = new Colors();
+        String result = Colors.getColor();
+        System.out.println("Result: " + result);
+    }
+}
+
+
+
+
