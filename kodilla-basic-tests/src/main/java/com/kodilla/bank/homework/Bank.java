0f767ac1af9d6d0a8e824d234d0f6865b83b1bf8
Natalia Mikolcz <nmikolcz@gmail.com>
2020-11-08 21:14:54 +0100

    Homework Bank without tests

--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
@@ -1,4 +1,53 @@
 package com.kodilla.bank.homework;
 
 public class Bank {
+    private String bank;
+    public CashMachine deposite;
+    public CashMachine paycheck;
+
+    public Bank (String cachMachinName) {
+        this.bank = cachMachinName;
+        this.deposite = new CashMachine();
+        this.paycheck = new CashMachine();
+    }
+
+    public void addDepositeValue(int value) {
+        if (value > 0 && value < 1000) {
+            this.deposite.add(value);
+        }
+    }
+
+    public void subPaycheckValue(int value) {
+        if (value > 0 && value < 1000) {
+            this.paycheck.sub(value);
+        }
+    }
+
+    public double getDepositeAverage() {
+        return this.deposite.getAverage();
+    }
+
+    public double getPaycheckAvarage() {
+        return this.paycheck.getAverage();
+    }
+
+    public double getBalance() {
+        double sum = this.deposite.getAverage() - this.paycheck.getAverage();
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        CashMachine cashMachine = new CashMachine();
+        Bank bank = new Bank("Pierwszy");
+        bank.addDepositeValue(900);
+        cashMachine.add(100);
+        cashMachine.add(250);
+        cashMachine.sub(-80);
+        cashMachine.sub(-100);
+        System.out.println("Bilans ze wszystkich bankomatów wynosi: " + bank.getBalance());
+        System.out.println("Liczba transakcji zwiazana z wpłatą wynosi: " + cashMachine.getTransaction());
+        System.out.println("Liczba transakcji zwiazana z wypłatą wynosi: " + cashMachine.getTransaction());
+        System.out.println("Średnia wartość wypłat wynosi: " + bank.getPaycheckAvarage());
+        System.out.println("Średnia wartość wpłat wynosi: " + bank.getDepositeAverage());
+    }
 }
--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMachine.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMachine.java
@@ -4,26 +4,49 @@ import java.util.Arrays;
 
 public class CashMachine {
 
-    private int [] values;
+    private int [] transaction;
     private int size;
 
     public CashMachine(){
         this.size=0;
-        this.values = new int[0];
+        this.transaction = new int[0];
     }
 
-    public void add(int value) {
+    public void add(int deposite) {
         this.size++;
         int[] depositeTab = new int[this.size];
-        System.arraycopy(values, 0, depositeTab, 0, values.length);
-        depositeTab[this.size -1] = value;
-        this.values = depositeTab;
+        System.arraycopy(transaction, 0, depositeTab, 0, transaction.length);
+        depositeTab[this.size -1] = deposite;
+        this.transaction = depositeTab;
+    }
+
+    public void sub(int paycheck){
+        this.size--;
+        int[] paycheckTab = new int[this.size];
+        System.arraycopy(transaction,0,paycheckTab,0,transaction.length);
+        paycheckTab[this.size -1] = paycheck;
+        this.transaction = paycheckTab;
     }
 
 
 
     public int[] getValues() {
-        return values;
+        return transaction;
     }
 
+    public int[] getTransaction() {
+        return transaction;
+    }
+
+
+    public double getAverage() {
+        if (this.transaction.length == 0) {
+            return 0;
+        }
+        double sum = 0;
+        for (int i = 0; i < this.transaction.length; i++) {
+            sum += this.transaction[i];
+        }
+        return sum/this.transaction.length;
+    }
 }
