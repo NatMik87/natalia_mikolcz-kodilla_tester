92fb557c1a0f7f92337e80ac997b2f11b3d90dac
Natalia Mikolcz <nmikolcz@gmail.com>
2020-11-29 18:51:00 +0100

    update

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -0,0 +1,10 @@
+package com.kodilla.collections.adv.immutable.special.homework;
+
+public class BookApplication {
+    public static void main(String[] args) {
+        System.out.println(BookManager.);
+        Book book = new Book("Harry Potter","J.K. Rowlling");
+
+
+    }
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -0,0 +1,12 @@
+package com.kodilla.collections.adv.immutable.special.homework;
+
+public class BookManager {
+    private String title;
+    private String author;
+
+    public Book createBook(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+
+}
