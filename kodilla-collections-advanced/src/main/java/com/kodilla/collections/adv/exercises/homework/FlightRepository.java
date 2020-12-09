180484eb8ed14f19b10c5a8d8886e16406626016
Natalia Mikolcz <nmikolcz@gmail.com>
2020-12-09 14:12:07 +0100

    fix for FlightRepository

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightRepository.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightRepository.java
@@ -6,17 +6,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FlightRepository {
-    public static void main(String[] args) {
+    public static List<Flight> getFlightTable() {
         List<Flight> fligths = new ArrayList<>();
         fligths.add(new Flight("Warszawa", "Singapur"));
         fligths.add(new Flight("Madryt", "Bogota"));
         fligths.add(new Flight("Amsterdam", "Chicago"));
         fligths.add(new Flight("Warszawa", "Ateny"));
+        return fligths;
     }
-
-        public String getFlightsTable(String departure) {
-            return departure ;
-        }
-
-
 }
+
+
+
+
