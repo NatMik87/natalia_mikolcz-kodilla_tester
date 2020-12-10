7913a6ca7afcd003d1664f985a86d0aee71acfd8
Natalia Mikolcz <nmikolcz@gmail.com>
2020-12-10 11:16:57 +0100

    streamUpdate

--- kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
@@ -1,11 +1,21 @@
 package com.kodilla.stream;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UsersManager {
     public static void main(String[] args) {
-        UserRepository.getUsersList()
-                .stream()
-                .map(u -> u.getUsername())
-                .forEach(un -> System.out.println(un));
+          List<String> usernames = UserRepository.getUsersList()
+                  .stream()
+                  .filter(u -> u.getGroup().equals("Chemists"))
+                  .map(UsersManager::getUserName)
+                  .collect(Collectors.toList());
+        }
+
+
+    public static String getUserName(User user) {
+        return user.getUsername();
     }
-    }
+
+}
 
