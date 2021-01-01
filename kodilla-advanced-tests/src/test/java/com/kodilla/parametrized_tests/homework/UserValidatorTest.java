b0818f47ac7f0cf091252caad52bdc89dff8e62c
Natalia Mikolcz <nmikolcz@gmail.com>
2021-01-01 12:24:56 +0100

    validateemail

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTest.java
@@ -0,0 +1,36 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserValidatorTest {
+    private UserValidator validator = new UserValidator();
+
+    @ParameterizedTest
+    @CsvSource(value = {"anna", "Hero001", "007_JB", "Smith-Wells"})
+    public void shouldReturnTrueIfNameValidate(String username , String expected) {
+        assertTrue(validator.validateUsername(username));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void shouldReturnFalseIfNameIsEmpty(String username) {
+        assertTrue(validator.validateUsername(username));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"@@@@" , "+.+", "  %%%  "})
+    public void shouldReturnFalseIfNameNotValidate(String username, String expected) {
+        assertFalse(validator.validateUsername(username));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"anna@op.pl", "hero_001@us.us", "james@007.gb", "smith-wells@suite.io"})
+    public void shouldReturnTrueIfEmailValidate(String email, String expected) {
+        assertTrue(validator.validateEmail(email));
+    }
+
+}
