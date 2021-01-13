8d356cd75aedadd70bac65f842e8e53dde669f9c
Natalia Mikolcz <nmikolcz@gmail.com>
2021-01-13 13:04:46 +0100

    bmi update

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/WeightSources.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/WeightSources.java
@@ -8,12 +8,17 @@ public class WeightSources {
 
     static Stream<Arguments> provideWeightForTestingBmi(){
         return Stream.of(
-                Arguments.of(170, 60, ""),
-                Arguments.of(100, 180),
-                Arguments.of(80, 175),
-                Arguments.of(50, 168),
-                Arguments.of(90, 155),
-                Arguments.of(120, 190)
+                Arguments.of(170, 45, "Very severely underweight"),
+                Arguments.of(180, 55, "Severely underweight"),
+                Arguments.of(165, 49, "Underweight"),
+                Arguments.of(168, 57, "Normal (healthy weight)"),
+                Arguments.of(170, 80, "Overweight"),
+                Arguments.of(170, 100, "Obese Class I (Moderately obese)"),
+                Arguments.of(170, 110, "Obese Class II (Severely obese)"),
+                Arguments.of(170, 120, "Obese Class III (Very severely obese)"),
+                Arguments.of(170, 140, "Obese Class IV (Morbidly Obese)"),
+                Arguments.of(170, 200, "Obese Class V (Super Obese)"),
+                Arguments.of(170, 350, "Obese Class VI (Hyper Obese)")
         );
     }
 }
