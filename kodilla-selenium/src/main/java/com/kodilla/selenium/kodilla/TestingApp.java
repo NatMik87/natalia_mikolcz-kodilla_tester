e99707888e330393ab26c1532872a7ea60fbd146
Natalia Mikolcz <nmikolcz@gmail.com>
2021-01-28 17:33:03 +0100

    update3

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/kodilla/TestingApp.java
@@ -0,0 +1,17 @@
+package com.kodilla.selenium.kodilla;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrom.driver" , "/Users/nataliamikolcz/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://kodilla.com/pl/sign-in");
+
+        WebElement inputField = driver.findElement(By.xpath("//html/body/div/div/form/fieldset/input"));
+        inputField.sendKeys("testuser@gmail.com");
+    }
+}
